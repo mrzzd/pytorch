156391d27a95d3c7e29275ac2bf6b431dcbedb25
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-08-24 15:04:24 -0700

    Adding alpha overload for add/sub ops with lists
    ghstack-source-id: b65f29f03e4a5802693e9aaca050c1c0fef49366
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43413

--- aten/src/ATen/native/ForeachUtils.h
+++ aten/src/ATen/native/ForeachUtils.h
@@ -15,6 +15,7 @@ void verify_list(TensorList tensors) {
 }
 
 // To go via 'fast' path, several conditions must be satisfied
+// - All tensors must be on the same device
 // - All tensors must have strided layout
 // - All tensors must be non-overlapping and dense
 // - Resulting tensor must have the same dtype as the input one
@@ -28,6 +29,10 @@ bool check_fast_route(TensorList tensors, Scalar scalar) {
     }
 
     if (t.layout() != at::kStrided) {
+      return false;
+    }
+
+    if (t.device() != expected_device) {
       return false;
     }
 
@@ -54,31 +59,53 @@ bool check_fast_route(TensorList tensors, Scalar scalar) {
   return true;
 }
 
+bool check_fast_route(TensorList tensors) {
+  TORCH_CHECK(tensors.size() > 0, "Tensor list must have at least one tensor.");
+  auto expected_device = tensors[0].device();
+
+   for (auto t : tensors) {
+    if (t.layout() != at::kStrided) {
+      return false;
+    }
+
+    if (!t.is_non_overlapping_and_dense()) {
+      return false;
+    }
+
+    if (t.device() != expected_device) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 void verify_list(TensorList tensors1, TensorList tensors2) {
   TORCH_CHECK(tensors1.size() > 0, "Tensor list must have at least one tensor.");
   TORCH_CHECK(tensors2.size() > 0, "Tensor list must have at least one tensor.");
   TORCH_CHECK(tensors1.size() == tensors2.size(), "Tensor lists must have the same number of tensors.");
 
   auto expected_dtype = tensors1[0].dtype();
-  auto expected_device = tensors1[0].device();
-
   for (int i = 0; i < tensors1.size(); i++) {
     TORCH_CHECK(tensors1[i].dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
-    TORCH_CHECK(tensors1[i].device() == expected_device, "All tensors in the tensor list must have the same device.");
-    
     TORCH_CHECK(tensors2[i].dtype() == expected_dtype, "All tensors in the tensor list must have the same dtype.");
-    TORCH_CHECK(tensors2[i].device() == expected_device, "All tensors in the tensor list must have the same device.");
-
     TORCH_CHECK(tensors1[i].sizes() == tensors2[i].sizes(), "Corresponding tensors in lists must have the same size.");
   }
 }
 
 bool check_fast_route(TensorList tensors1, TensorList tensors2) {
+  auto expected_device = tensors1[0].device();
+
   for (int64_t i = 0; i < tensors1.size(); i++) {
     TORCH_CHECK(tensors1[i].sizes() == tensors2[i].sizes(), "Corresponding tensors from tensor lists have different size.");
 
     if (tensors1[i].layout() != at::kStrided || 
         tensors2[i].layout() != at::kStrided) {
+      return false;
+    }
+
+    if (tensors1[i].device() != expected_device || 
+        tensors2[i].device() != expected_device) {
       return false;
     }
 
